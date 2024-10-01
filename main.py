ddda1b89ac80b91c8a0f8193407aee194448a544
Fahreza Dhia <fahrezadhia16@gmail.com>
2024-10-01 16:26:29 +0700

    adding pseudocode

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 # a script to find duplicate files either by name or checksum in a folder
+# the checksum algorithm is Adler-32
+# the flow :
+# 1. choose directory
+# 2. list all the files in the choosen directory
+# 3. checksum every file in the choosen directory 
+# 4. store the checksum value in a list
+# 5. check the list for possible duplicate
+# 6. delete the duplicate data
+# 7. finish 
+
 from functions import *
 import os
 
